d1924cd2bb1b6084ab7635225562a84ede67306f
mitriadamian <89418934+mitriadamian@users.noreply.github.com>
2021-10-08 14:48:16 -0300

    primer commit

---
+++ test/ar/edu/unlam/test/TestVideoClub.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestVideoClub {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
