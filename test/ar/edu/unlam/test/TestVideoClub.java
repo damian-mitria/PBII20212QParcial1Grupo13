da36c2f8c888dd33e4a208ecdec3c750fb3e577b
celeste Aguirre <celaguirre@alumno.unlam.edu.ar>
2021-10-08 18:17:02 -0300

    primer testeo

--- test/ar/edu/unlam/test/TestVideoClub.java
+++ test/ar/edu/unlam/test/TestVideoClub.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import ar.edu.unlam.dominio.Cliente;
+import ar.edu.unlam.dominio.Socio;
+import ar.edu.unlam.dominio.VideoClub;
+
 public class TestVideoClub {
 
 	@Test
-	public void test() {
+	public void queSePuedaAgregarUnClienteALaLista() {
+	VideoClub videoClub = new VideoClub("Movies");
+	Socio cliente1 = new Socio("Celeste", 4200000);
+	
+	assertTrue(videoClub.agregarCliente(cliente1));
 	
 	}
 
