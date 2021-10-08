a475ddf1831d3292decc12b0ddc8d4ce3b158287
unuusual <alarganaraz@alumno.unlam.edu.ar>
2021-10-08 15:41:29 -0300

    cuarto commit transaccion y parte del videoClub

--- src/ar/edu/unlam/dominio/Transaccion.java
+++ src/ar/edu/unlam/dominio/Transaccion.java
@@ -1,5 +1,54 @@
 package ar.edu.unlam.dominio;
 
 public class Transaccion {
+	private Pelicula pelicula;
+	private Cliente cliente;
+	private Integer id;
+	private TipoDeTransaccion tipoDeTransaccion;
 
+	public Transaccion(Pelicula pelicula, Cliente cliente, TipoDeTransaccion tipoDeTransaccion) {
+
+		this.pelicula = pelicula;
+		this.cliente = cliente;
+		this.tipoDeTransaccion = tipoDeTransaccion;
+		this.setId((int) Math.random() * 1000);
+	}
+
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public TipoDeTransaccion getTipoDeTransaccion() {
+		return tipoDeTransaccion;
+	}
+
+	public void setTipoDeTransaccion(TipoDeTransaccion tipoDeTransaccion) {
+		this.tipoDeTransaccion = tipoDeTransaccion;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaccion [pelicula=" + pelicula + ", cliente=" + cliente + ", id=" + id + ", tipoDeTransaccion="
+				+ tipoDeTransaccion + "]";
+	}
 }
